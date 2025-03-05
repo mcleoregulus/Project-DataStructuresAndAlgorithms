75bdbec9aa9225f6a36cc91ff84bc073157d7c57
LeoRegulus <mcleoregulus@outlook.com>
2025-03-05 23:16:40 +0800

    pointer2vector done

--- include/Graphm.cpp
+++ include/Graphm.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <cassert>
 #include <complex>
-// #include <vector>
+#include <vector>
 
 using namespace std;
 
@@ -12,10 +12,10 @@ class Graphm : public Graph
 {
 private:
     int numVertex, numEdge; // Store number of vertices, edges
-    int *mark;
+    vector<int> mark;       // Use vector for mark array
 
-    complex<double> **matrix;       // Pointer to adjacency matrix
-    complex<double> **admit_matrix; // Pointer to admittance matrix
+    vector<vector<complex<double>>> matrix;       // Use vector of vectors for adjacency matrix
+    vector<vector<complex<double>>> admit_matrix; // Use vector of vectors for admittance matrix
 
 public:
     Graphm(int numVert)
@@ -24,42 +24,21 @@ public:
     }
     ~Graphm()
     {
-        // Pointer to mark array
-        // Constructor
         // Destructor
-        delete[] mark; // Return dynamically allocated memory
-
-        for (int i = 0; i < numVertex; i++)
-            delete[] matrix[i];
-        delete[] matrix;
-
-        for (int i = 0; i < numVertex; i++)
-            delete[] admit_matrix[i];
-        delete[] admit_matrix;
+        // No need to manually delete memory for vectors
     }
-    void Init(int n)    // Initialize the graph
+    void Init(int n) // Initialize the graph
     {
         int i;
         numVertex = n;
         numEdge = 0;
-        mark = new int[n];
-        // Initialize mark array
-        for (i = 0; i < numVertex; i++)
-            mark[i] = 0;                       // UNVISITED = 0
-        
-        matrix = (complex<double> **)new complex<double> *[numVertex]; // Make matrix
-        for (i = 0; i < numVertex; i++)
-            matrix[i] = new complex<double>[numVertex];
-        for (i = 0; i < numVertex; i++) // Initialize to 0 weights
-            for (int j = 0; j < numVertex; j++)
-                matrix[i][j] = 0;                                           // !!!
-        
-        admit_matrix = (complex<double> **)new complex<double> *[numVertex]; // Make matrix
-        for (i = 0; i < numVertex; i++)
-            admit_matrix[i] = new complex<double>[numVertex];
-        for (i = 0; i < numVertex; i++) // Initialize to 0
-            for (int j = 0; j < numVertex; j++)
-                admit_matrix[i][j] = {0, 0};
+        mark.resize(n, 0); // Initialize mark array with 0 (UNVISITED)
+
+        // Initialize adjacency matrix
+        matrix.resize(numVertex, vector<complex<double>>(numVertex, complex<double>(0, 0)));
+
+        // Initialize admittance matrix
+        admit_matrix.resize(numVertex, vector<complex<double>>(numVertex, complex<double>(0, 0)));
     }
     int n() { return numVertex; } // Number of vertices
     int e() { return numEdge; }   // Number of edges
@@ -67,7 +46,7 @@ public:
     int first(int v)
     {
         for (int i = 0; i < numVertex; i++)
-            if (matrix[v][i] != complex<double> {0,0}) // ?????
+            if (matrix[v][i] != complex<double>(0, 0))
                 return i;
         return numVertex;
         // Return n if none
@@ -76,7 +55,7 @@ public:
     int next(int v, int w)
     {
         for (int i = w + 1; i < numVertex; i++)
-            if (matrix[v][i] != complex<double> {0,0}) // ?????
+            if (matrix[v][i] != complex<double>(0, 0))
                 return i;
         return numVertex;
         // Return n if none
@@ -84,29 +63,29 @@ public:
     // Set edge (v1, v2) to "wt"
     void setEdge(int v1, int v2, complex<double> wt)
     {
-        // Assert(wt>0, "Illegal weight value");            // use cassert
+        // Assert(wt>0, "Illegal weight value");               // use cassert
         // assert(real(wt) > 0 && "Illegal weight value");     // real part need to be positive
-        if (matrix[v1][v2] == complex<double> {0,0})        // ???
+        if (matrix[v1][v2] == complex<double>(0, 0)) // ???
             numEdge++;
         matrix[v1][v2] = wt;
     }
     void delEdge(int v1, int v2)
     { // Delete edge (v1, v2)
-        if (matrix[v1][v2] != complex<double> {0,0})
+        if (matrix[v1][v2] != complex<double>(0, 0))
             numEdge--;
-        matrix[v1][v2] = complex<double> {0,0};
+        matrix[v1][v2] = complex<double>(0, 0);
     }
     bool isEdge(int i, int j) // Is (i, j) an edge?
     {
-        return matrix[i][j] != complex<double> {0,0};
+        return matrix[i][j] != complex<double>(0, 0);
     }
-    complex<double> weight(int v1, int v2) { return matrix[v1][v2]; }   // changed type in ADT
+    complex<double> weight(int v1, int v2) { return matrix[v1][v2]; } // changed type in ADT
     int getMark(int v) { return mark[v]; }
     void setMark(int v, int val) { mark[v] = val; }
 
-    complex<double> **adjMatrix() { return matrix; }
+    vector<vector<complex<double>>> adjMatrix() { return matrix; }
 
-    complex<double> **admitMatrix()
+    vector<vector<complex<double>>> admitMatrix()
     {
         for (int i = 0; i < numVertex; i++)
         {
@@ -127,13 +106,15 @@ public:
         return admit_matrix;
     }
 
-    void printMatrix(complex<double> **matrix) {
-        for (int i = 0; i < numVertex; i++) {
-            for (int j = 0; j < numVertex; j++) {
+    void printMatrix(vector<vector<complex<double>>> matrix)
+    {
+        for (int i = 0; i < numVertex; i++)
+        {
+            for (int j = 0; j < numVertex; j++)
+            {
                 cout << matrix[i][j] << "\t";
             }
-            cout << endl; 
-        }    
+            cout << endl;
+        }
     }
-    
 };
